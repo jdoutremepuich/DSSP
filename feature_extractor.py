ef7171e563e1b742a800342985089a373309c062
jdoutremepuich <jdoutremepuich@gmail.com>
2015-07-04 16:21:36 +0200

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -17,15 +17,8 @@ class FeatureExtractor(object):
         special_days=pd.read_csv(os.path.join(path, "data_specialdays.csv"), sep = ';')
         X_encoded = X_encoded.merge(special_days, how='left', left_on=['DateOfDeparture'], right_on=['DateOfDeparture'], sort=False)
         
-        #uncomment the line below in the submission
-        #path = os.path.dirname(__file__)
-        #special_days=pd.read_csv(os.path.join(path, "data_specialdays.csv"), sep = ';')
-        special_days=pd.read_csv("data_specialdays.csv", sep = ';')
-        X_encoded = X_encoded.merge(special_days, how='left', left_on=['DateOfDeparture'], right_on=['DateOfDeparture'], sort=False)
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Departure'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Arrival'], prefix='a'))
-        X_encoded = X_encoded.drop('Departure', axis=1)
-        X_encoded = X_encoded.drop('Arrival', axis=1)     
         
         X_encoded['DateOfDeparture'] = pd.to_datetime(X_encoded['DateOfDeparture'])
         X_encoded['year'] = X_encoded['DateOfDeparture'].dt.year
@@ -35,15 +28,13 @@ class FeatureExtractor(object):
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['weekday'], prefix='wd'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['week'], prefix='w'))
         
-        
-                
+        X_encoded = X_encoded.drop('Departure', axis=1)
+        X_encoded = X_encoded.drop('Arrival', axis=1) 
         X_encoded = X_encoded.drop('weekday', axis=1)
         X_encoded = X_encoded.drop('week', axis=1)
         X_encoded = X_encoded.drop('year', axis=1)
         X_encoded = X_encoded.drop('std_wtd', axis=1)
         X_encoded = X_encoded.drop('WeeksToDeparture', axis=1)        
         X_encoded = X_encoded.drop('DateOfDeparture', axis=1)
-        X_encoded = X_encoded.drop('ISSPECIALDAY', axis=1)
-        X_encoded = X_encoded.drop('ISAROUNDSPECIALDAY', axis=1)
         X_array = X_encoded.values
         return X_array
